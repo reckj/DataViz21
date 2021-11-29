c7bd581755188f1e50eadda63cd6ad25707ae275
reckj <reck.johannes@gmail.com>
2021-11-29 18:16:26 +0100

    technical system setup

--- sketch.js
+++ sketch.js
@@ -1,36 +1,46 @@
-//const api_url = 'https://covid19-api.com/country/code?code=CH&format=json'
-//let user_data;
-
 let screenWidth = 3840;
 let screenHeight = 2160;
 let screenScaler = 2; //scaler for work on smaller screens
 
-const hammerPlayground = document.getElementById('hammer');
-const hammerOptions = {};
-const mc = new Hammer(hammerPlayground, hammerOptions);
+let interActionAreaWidth = screenWidth / 4;
+let interActionAreaHeight = screenHeight / 12;
+
+let touchColor = 0;
+
+let ringRadius1 = screenWidth * 1 / 4;
+let ringRadius2 = screenWidth * 1 / 3;
+let ringRadius3 = screenWidth * 1 / 2;
 
 
 function setup() {
   //createCanvas(window.innerWidth, window.innerHeight);
   createCanvas(screenWidth/screenScaler, screenHeight/screenScaler);
-  background(255);
+  background(5);
   textSize(22);
-
-  /*
-  httpGet(api_url, 'json', false, function (response) {
-    user_data = response;
-    console.log(user_data);
-  });
-  */
-}
-
-
-function draw() {
-  mc.on('singletap doubletap', (event) => {
-    //hammerPlayground.textContent = event.type + ' ';
-    console.log('event: ', event);
-  });
   
 }
 
 
+function draw() {
+  
+  let ringColor3 = color(220, 180, 0);
+  fill(ringColor3);
+  circle(width/2, height, ringRadius3);
+
+  let ringColor2 = color(200, 200, 0);
+  fill(ringColor2);
+  circle(width/2, height, ringRadius2);
+
+  let ringColor1 = color(200, 255, 0);
+  fill(ringColor1);
+  circle(width/2, height, ringRadius1);
+
+  let c = color(touchColor, 0, 0);
+  fill(c);
+  rectMode(CENTER);
+  rect(width/2, height - interActionAreaHeight/2, interActionAreaWidth, interActionAreaHeight);
+}
+
+function touchMoved() {
+  touchColor = mouseX/10 % 255;
+}
