2a2ae6e02977cf10cce917fa4513dd8989c019e7
reckj <reck.johannes@gmail.com>
2021-11-29 16:28:18 +0100

    tried some drawing stuff to check functionality

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ const api_url = 'https://covid19-api.com/country/code?code=CH&format=json'
 
 var user_data;
 let circleSize = 0;
+let rotation = 0;
+let colorwheel = 0;
 
 function preload() {
   //uncomment this to do the same using fetch() API
@@ -28,11 +30,12 @@ function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(125);
   textSize(22);
-  ctx = document.getElementById('co2_CH').getContext('2d');
+  //ctx = document.getElementById('co2_CH').getContext('2d');
 
   httpGet(api_url, 'json', false, function (response) {
     user_data = response;
     //drawOnce();
+    console.log(user_data);
   });
 }
 
@@ -57,14 +60,27 @@ function draw() {
     background(125);
   }
 
+  if(rotation > 255) {
+    colorwheel = 0;
+  }
+
+  rotation = rotation + 0.1;
   //fill(10);
   //circle(width/2,height/2,circleSize + 50);
-  fill(0);
+  fill(255 -  colorwheel, 0,50 + colorwheel/2);
+  rectMode(CENTER);
+  push();
+  translate(width/2, height/2);
+  rotate(rotation);
+  rect(0, 0, circleSize,circleSize);
+  pop();
+  fill(20);
   circle(width/2,height/2,circleSize);
   fill(255);
   textSize(circleSize/2);
   textAlign(CENTER, CENTER);
   text(user_data[0].deaths, width/2, height/2);
+  
 
  /*
   if (!user_data) {
