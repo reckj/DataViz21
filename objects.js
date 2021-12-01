0ee4e03cdb262fdeaf722a3c8c5165303503ab7f
Janosch <github@janosch.com>
2021-12-01 14:11:38 +0100

    Added even more Objects

--- objects.js
+++ objects.js
@@ -34,11 +34,21 @@ screenObjects.push(screenObjectBuilder('mediaObjectVideo', [1760,1840], [160,160
 screenObjects.push(screenObjectBuilder('mediaObjectText', [2000,1840], [160,160], 'Text', 0.1));
 screenObjects.push(screenObjectBuilder('mediaObjectVoice', [2240,1840], [160,160], 'Voice', 1));
 
-screenObjects.push(screenObjectBuilder('receiverObjectPerson1', [400,1360], [160,160], 'Mica', 0));
-screenObjects.push(screenObjectBuilder('receiverObjectPerson2', [1440,1360], [160,160], 'Elena', 0));
-screenObjects.push(screenObjectBuilder('receiverObjectPerson3', [2640,1360], [160,160], 'Johannes', 0));
+screenObjects.push(screenObjectBuilder('receiverObjectPerson1', [400,1360], [160,160], 'Mica', 1));
+screenObjects.push(screenObjectBuilder('receiverObjectPerson2', [1440,1360], [160,160], 'Elena', 1));
+screenObjects.push(screenObjectBuilder('receiverObjectPerson3', [2640,1360], [160,160], 'Johannes', 1));
+screenObjects.push(screenObjectBuilder('receiverObjectPerson4', [3360,1360], [160,160], 'Janosch', 1));
+screenObjects.push(screenObjectBuilder('receiverObjectSmallGroup1', [2800,560], [240,240], 'Family', 5));
+screenObjects.push(screenObjectBuilder('receiverObjectSmallGroup2', [1280,640], [240,240], 'Friends', 8));
+screenObjects.push(screenObjectBuilder('receiverObjectBigGroup1', [640,400], [320,320], 'Jahrgang', 56));
+screenObjects.push(screenObjectBuilder('receiverObjectBigGroup2', [2240,320], [320,320], 'Fussballmannschaft', 24));
+screenObjects.push(screenObjectBuilder('receiverObjectWorld', [0,0], [3840,240], 'Social Media', 999));
 
+screenObjects.push(screenObjectBuilder('emissionObjectEnergy', [0,240], [480,480], 'Energieverbrauch', 100));
+screenObjects.push(screenObjectBuilder('emissionObjectWater', [3360,0], [480,480], 'Wasserverbrauch', 100));
+screenObjects.push(screenObjectBuilder('emissionObjectLand', [3360,1680], [480,480], 'Landverbrauch', 100));
 
+screenObjects.push(screenObjectBuilder('serverObjectLand', [160,1120], [3520,320], ' Internet Traffic', 0));
 
 //test screen objects
 console.log(screenObjects.find(element => element.name === 'testobject'));
